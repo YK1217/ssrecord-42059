a1b1c6bc90e70b86fc87e4c6ce3600ceca15f32e
YK1217 <bean.sousaku@gmail.com>
2026-04-13 15:59:08 +0900

    ユーザー登録機能のバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,26 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/
+
+  validates :name, presence: true, length: { maximum: 10 }
+
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  validates :password,
+            presence: true,
+            length: { minimum: 8 },
+            format: { with: VALID_PASSWORD_REGEX },
+            if: :password_required?
+
+  private
+
+  def password_required?
+    new_record? || password.present?
+  end
 end
