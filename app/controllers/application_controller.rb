1853bc71c79b9fb7250214918dc9ba00b332599c
YK1217 <bean.sousaku@gmail.com>
2026-04-15 10:31:27 +0900

    ログイン・ログアウト後の遷移先の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+
+  def after_sign_in_path_for(resource)
+    user_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
   private
 
   def configure_permitted_parameters
