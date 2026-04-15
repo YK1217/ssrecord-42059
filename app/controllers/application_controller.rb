c4d692159b2abeab8307b9ffc4c3d9ef21270f73
YK1217 <bean.sousaku@gmail.com>
2026-04-15 11:28:56 +0900

    ログイン・ログアウト後の遷移先を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
   protected
 
   def after_sign_in_path_for(resource)
-    user_path
+    root_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
-    root_path
+    new_user_session_path
   end
 
   private
