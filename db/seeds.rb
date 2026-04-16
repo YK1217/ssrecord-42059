45b1a3b7553bdc7be61aa021eb9834d151687796
YK1217 <bean.sousaku@gmail.com>
2026-04-16 14:08:43 +0900

    テストユーザー作成のシードデータを設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# テストログイン用ユーザーを作成する
+test_user = User.find_or_initialize_by(email: "test@example.ne.jp")
+test_user.name = "Test_User"
+test_user.password = ENV["TEST_PASSWORD"]
+test_user.password_confirmation = test_user.password
+test_user.save!
+
+puts "テストログイン用ユーザーを作成しました"
+puts "email: test@example.ne.jp"
+puts "name: Test_User"
