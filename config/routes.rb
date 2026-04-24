ca68ba203c97bb26a307df794bae2d5ede6d28f3
YK1217 <bean.sousaku@gmail.com>
2026-04-24 15:37:41 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'homes/top'
   devise_for :users
 
   root "homes#top"
   resource :user, only: [:show]
+  resources :study_records, only: [:new, :create]
 end
