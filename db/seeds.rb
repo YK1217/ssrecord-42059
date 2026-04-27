c946d95ce72b0f35e6fe694149aff0d6f421f4b5
YK1217 <bean.sousaku@gmail.com>
2026-04-27 15:38:38 +0900

    テストログイン用ユーザーに学習記録が3つ以上無ければ作るようシードデータを設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,21 @@ test_user.save!
 puts "テストログイン用ユーザーを作成しました"
 puts "email: test@example.ne.jp"
 puts "name: Test_User"
+
+  needed_count = 3 - test_user.study_records.size
+
+if needed_count > 0
+  puts "----------------------------------------"
+  puts "テストログイン用ユーザーに学習記録を #{needed_count}個作ります"
+
+  new_records = FactoryBot.create_list(:study_record, needed_count, user: test_user)
+
+  new_records.each do |new_record|
+    puts "\nstart_time: #{new_record.start_time}"
+    puts "end_time: #{new_record.end_time}"
+    puts "study_memo: #{new_record.study_memo}"
+  end
+
+  puts "\n学習記録を #{needed_count}個作成しました"
+
+end
