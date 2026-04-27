c0e8e3f898365cd76d76459e031913ca16ec92cb
YK1217 <bean.sousaku@gmail.com>
2026-04-27 12:31:53 +0900

    時刻を日本時間に設定

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ module Ssrecord42059
     # 日本語の言語設定
     config.i18n.default_locale = :ja
 
+    # 日本時間のタイムゾーン設定
+    config.time_zone = "Tokyo"
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
