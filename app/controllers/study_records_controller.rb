f416ec24e970f8b500b8c2ce2fef18fe0c4d4bd0
YK1217 <bean.sousaku@gmail.com>
2026-04-27 10:02:06 +0900

    study_recordsコントローラーのnew、createアクションの設定

--- app/controllers/study_records_controller.rb
+++ app/controllers/study_records_controller.rb
@@ -1,4 +1,23 @@
 class StudyRecordsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
+    @study_record = StudyRecord.new
+  end
+
+  def create
+    @study_record = current_user.study_records.new(study_record_params)
+
+    if @study_record.save
+      redirect_to user_path, notice: "学習時間を登録しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def study_record_params
+    params.require(:study_record).permit(:start_time, :end_clock, :study_memo)
   end
 end
