1397f999359607640a12b3c7610ba9cc4eecbf93
YK1217 <bean.sousaku@gmail.com>
2026-04-28 10:14:35 +0900

    シードデータの設定を調整

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,20 +9,38 @@ puts "テストログイン用ユーザーを作成しました"
 puts "email: test@example.ne.jp"
 puts "name: Test_User"
 
-  needed_count = 3 - test_user.study_records.size
+  if test_user.nil?
+    puts "テストログイン用ユーザーの作成に失敗しました"
+    exit
+  end
+
+  # 作っておきたい記録の数
+  target_count = 11
+
+  needed_count = target_count - test_user.study_records.size
 
 if needed_count > 0
   puts "----------------------------------------"
   puts "テストログイン用ユーザーに学習記録を #{needed_count}個作ります"
 
-  new_records = FactoryBot.create_list(:study_record, needed_count, user: test_user)
+  current_date = Date.yesterday
 
-  new_records.each do |new_record|
-    puts "\nstart_time: #{new_record.start_time}"
-    puts "end_time: #{new_record.end_time}"
-    puts "study_memo: #{new_record.study_memo}"
+  while test_user.study_records.size < target_count do
+    unless test_user.study_records.where(study_date: current_date).exists?
+      new_record = FactoryBot.build(:study_record, user: test_user)
+      new_record.start_time = new_record.start_time.change(year: current_date.year, month: current_date.month, day: current_date.day)
+      new_record.save!
+      puts "\nstart_time: #{new_record.start_time}"
+      puts "end_time: #{new_record.end_time}"
+      puts "study_memo: #{new_record.study_memo}"
+    end
+    current_date -= 2
+
+    if current_date < Date.current - 1.year
+      break
+    end
   end
 
-  puts "\n学習記録を #{needed_count}個作成しました"
+  puts "\n学習記録の作成を終了しました"
 
 end
