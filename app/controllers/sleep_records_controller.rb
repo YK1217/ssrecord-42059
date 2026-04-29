4185bfe46b33ad77f8fb1479df4a84c4701c90cd
YK1217 <bean.sousaku@gmail.com>
2026-04-29 11:14:42 +0900

    sleep_recordsコントローラーにnew、createアクションを設定

--- app/controllers/sleep_records_controller.rb
+++ app/controllers/sleep_records_controller.rb
@@ -1,7 +1,23 @@
 class SleepRecordsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
+    @sleep_record = SleepRecord.new
   end
 
   def create
+    @sleep_record = current_user.sleep_records.new(sleep_record_params)
+
+    if @sleep_record.save
+      redirect_to user_path, notice: "睡眠時間を登録しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def sleep_record_params
+    params.require(:sleep_record).permit(:start_time, :end_clock)
   end
 end
