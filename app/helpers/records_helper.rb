752d7b8fddc556df59e2d5080430e0a290ec5919
YK1217 <bean.sousaku@gmail.com>
2026-05-02 11:17:29 +0900

    日別一覧画面の表示を調整

---
+++ app/helpers/records_helper.rb
@@ -0,0 +1,32 @@
+module RecordsHelper
+
+  # 記録時間表示用
+  def format_record_time(minutes)
+    return incomplete_badge if minutes.blank?
+
+    hours = minutes / 60
+    remain_minutes = minutes % 60
+
+    if hours > 0
+      "#{hours}時間#{remain_minutes}分"
+    else
+      "#{remain_minutes}分"
+    end
+  end
+
+  # 終了時刻表示用
+  def format_end_time(end_time)
+    return incomplete_badge if end_time.blank?
+
+    l(end_time, format: :time)
+  end
+
+  # 未完了バッジ
+  def incomplete_badge
+    content_tag(
+      :span,
+      "未完了",
+      class: "badge bg-warning text-dark"
+    )
+  end
+end
