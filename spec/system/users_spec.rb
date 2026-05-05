7005affd13815fbb36e997eb621f7bdf309fe167
YK1217 <bean.sousaku@gmail.com>
2026-05-05 11:06:38 +0900

    ユーザー新規登録の結合テストコードファイルの作成

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :system do
+  before do
+    @user = FactoryBot(:user)
+  end
+
+  context 'ユーザー新規登録ができる時' do
+    it '正しい情報を入力すればユーザー新規登録ができてトップページに遷移する' do
+
+    end
+  end
+  context 'ユーザー新規登録ができない時' do
+    it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
+
+    end
+  end
+end
