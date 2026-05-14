ab7a6c5784db63e36dd165b0155a28dc31d15516
YK1217 <bean.sousaku@gmail.com>
2026-05-14 16:15:18 +0900

    学習/睡眠時間のテキストを算出する部分テンプレートを追加

--- spec/support/time_support.rb
+++ spec/support/time_support.rb
@@ -21,6 +21,24 @@ module TimeSupport
     end
   end
 
+  def build_time_difference_from(record)
+    if record.end_time.blank?
+      return ''
+    end
+
+    end_time = build_next_day_end_time_from(record)
+    diff = end_time - record.start_time
+    minutes = (diff / 60).to_i
+    hours = minutes / 60
+    remain_minutes = minutes % 60
+
+    if hours > 0
+      "#{hours}時間#{remain_minutes}分"
+    else
+      "#{remain_minutes}分"
+    end
+  end
+
   def build_end_clock_from(record)
     return record.end_time&.strftime("%H:%M") || ''
   end
--- spec/system/study_records_spec.rb
+++ spec/system/study_records_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe "学習時間登録", type: :system do
       visit new_study_record_path
       # フォームに情報を入力する
       fill_in '学習開始日時', with: @study_record.start_time
-      fill_in '学習終了時刻', with: @study_record.end_clock
+      fill_in '学習終了時刻', with: Time.zone.parse(@study_record.end_clock)
       fill_in '学習内容', with: @study_record.study_memo
       # 送信するとトップページに遷移し、StudyRecordモデルのカウントが1上がることを確認する
       expect{
@@ -24,17 +24,19 @@ RSpec.describe "学習時間登録", type: :system do
         expect(page).to have_current_path(root_path)
       }.to change { StudyRecord.count }.by(1)
       # トップページには先ほど登録した学習時間記録の日付が表示されているカードが存在することを確認する
-      study_date = I18n.l(@study_record.start_time.to_date,format: :long)
-      expect(page).to have_selector(".card-header",text: study_date)
+      study_date_text = I18n.l(@study_record.start_time.to_date,format: :long)
+      expect(page).to have_selector(".card-header",text: study_date_text)
 
       # 日付が表示されているカード内に先ほど登録した学習時間記録の内容が表示されていることを確認する
 
       end_time = build_end_time_from(@study_record)
+      study_time_text = build_time_difference_from(@study_record)
 
-      within(".card", text: study_date) do
+      within(".card", text: study_date_text) do
         expect(page).to have_content('学習時間')
         expect(page).to have_content(I18n.l(@study_record.start_time,format: :time))
         expect(page).to have_content(I18n.l(end_time, format: :time))
+        expect(page).to have_content study_time_text
         expect(page).to have_content(@study_record.study_memo)
       end
     end
@@ -80,8 +82,9 @@ RSpec.describe '学習時間編集' do
       # 投稿内容を編集する
       new_study_date = @study_record.start_time.to_date - 1.day
       new_start_time = Time.zone.local(new_study_date.year, new_study_date.month, new_study_date.day, 9, 0)
-      new_end_clock = "15:00"
+      new_end_clock = Time.zone.parse("15:00")
       new_end_time = Time.zone.local(new_study_date.year, new_study_date.month, new_study_date.day, 15, 0)
+      new_study_time_text = "6時間0分"
       new_study_memo = "テストメモ"
 
       fill_in '学習開始日時', with: new_start_time
@@ -100,6 +103,7 @@ RSpec.describe '学習時間編集' do
         expect(page).to have_content('学習時間')
         expect(page).to have_content(I18n.l(new_start_time,format: :time))
         expect(page).to have_content(I18n.l(new_end_time, format: :time))
+        expect(page).to have_content new_study_time_text
         expect(page).to have_content new_study_memo
       end
     end
