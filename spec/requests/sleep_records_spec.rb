b28056071c4e1f603fbad119e94ab7399d92f03e
YK1217 <bean.sousaku@gmail.com>
2026-06-10 14:11:51 +0900

    newアクションのexampleを整理

--- spec/requests/sleep_records_spec.rb
+++ spec/requests/sleep_records_spec.rb
@@ -1,10 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe "SleepRecords", type: :request do
-  describe "GET /sleep_records" do
-    it "works! (now write some real specs)" do
-      get sleep_records_index_path
-      expect(response).to have_http_status(200)
+  let(:user) { FactoryBot.create(:user) }
+
+  describe "GET #new" do
+    context 'ログインしている場合' do
+      before do
+        sign_in user
+        get new_sleep_record_path
+      end
+
+      it 'newアクションにリクエストすると正常にレスポンスが返ってくる' do
+      end
+      it 'newアクションにリクエストすると睡眠時間登録フォームが表示される' do
+      end
+      it 'newアクションにリクエストすると就寝時間の入力欄が表示される' do
+      end
+      it 'newアクションにリクエストすると起床時刻の入力欄が表示される' do
+      end
+    end
+    context 'ログインしていない場合' do
+      before do
+        get new_sleep_record_path
+      end
+
+      it 'newアクションにリクエストするとログインページにリダイレクトされる' do
+      end
     end
   end
 end
