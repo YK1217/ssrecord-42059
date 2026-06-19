212e274a7416490661ed7c52b4e838669e059125
YK1217 <bean.sousaku@gmail.com>
2026-06-19 13:36:12 +0900

    newアクションにおけるログインしている場合の設定を追加

--- spec/requests/study_records_spec.rb
+++ spec/requests/study_records_spec.rb
@@ -11,14 +11,22 @@ RSpec.describe "StudyRecords", type: :request do
       end
 
       it 'newアクションにリクエストすると正常にレスポンスが返ってくる' do
+        expect(response).to have_http_status(:ok)
       end
       it 'newアクションにリクエストすると学習時間登録フォームが表示される' do
+        html = Nokogiri::HTML(response.body)
+
+        expect(html.at_css('h1').text).to include("学習時間登録")
+        expect(html.at_css('form')).to be_present
       end
-      it 'newアクションにリクエストすると学習開始時間の入力欄が表示される' do
+      it 'newアクションにリクエストすると学習開始日時の入力欄が表示される' do
+        expect(response.body).to include("学習開始日時")
       end
       it 'newアクションにリクエストすると学習終了時刻の入力欄が表示される' do
+        expect(response.body).to include("学習終了時刻")
       end
       it 'newアクションにリクエストすると学習メモの入力欄が表示される' do
+        expect(response.body).to include("学習内容")
       end
     end
 
