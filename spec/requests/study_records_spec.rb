a5a1828cd1205bf6677c42e8efef131bda5bec4c
YK1217 <bean.sousaku@gmail.com>
2026-06-19 13:32:31 +0900

    newアクションのexampleを整理

--- spec/requests/study_records_spec.rb
+++ spec/requests/study_records_spec.rb
@@ -1,10 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe "StudyRecords", type: :request do
-  describe "GET /study_records" do
-    it "works! (now write some real specs)" do
-      get study_records_index_path
-      expect(response).to have_http_status(200)
+  let(:user) { create(:user) }
+
+  describe 'GET #new' do
+    context 'ログインしている場合' do
+      before do
+        sign_in user
+        get new_study_record_path
+      end
+
+      it 'newアクションにリクエストすると正常にレスポンスが返ってくる' do
+      end
+      it 'newアクションにリクエストすると学習時間登録フォームが表示される' do
+      end
+      it 'newアクションにリクエストすると学習開始時間の入力欄が表示される' do
+      end
+      it 'newアクションにリクエストすると学習終了時刻の入力欄が表示される' do
+      end
+      it 'newアクションにリクエストすると学習メモの入力欄が表示される' do
+      end
+    end
+
+    context 'ログインしていない場合' do
+      it 'newアクションにリクエストするとログイン画面へリダイレクトされる' do
+      end
     end
   end
 end
